50dbecb9aa48d9575b3e71794c35f15e0e73e90e
Vanya Yaneva <wyaneva@gmail.com>
2017-04-28 11:47:00 +0100

    fixed a bug

--- test/paper-examples/freq/freq.c
+++ test/paper-examples/freq/freq.c
@@ -4,23 +4,23 @@
 
 int main(int argc, char* argv[])
 {
-  char str[1000], ch;
+  char str[1000], ch[2];
 
   printf("Enter a string: ");
   fgets(str, 1000, stdin);
   printf("Enter a character: ");
-  fgets(&ch, 2, stdin);
+  fgets(ch, 2, stdin);
 
   char* str_ptr = str;
   int occurs = 0;
   while(*str_ptr != '\0')
   {
-    if(*str_ptr == ch)
+    if(*str_ptr == ch[0])
     {
       occurs++;
     }
     str_ptr++;
   }
 
-  printf("'%c' occurs %d times in the string.\n", ch, occurs);
+  printf("'%c' occurs %d times in the string.\n", ch[0], occurs);
 }
